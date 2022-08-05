e432bb5b60f6a2b948538594218b5c386eaaad13
Alexandre Fourcat <afourcat@gmail.com>
2022-08-05 11:55:57 +0200

    :recycle: Replace manually create role policy for cloudwatch with BasicExecutionRole.

--- main.tf
+++ main.tf
@@ -93,12 +93,6 @@ resource "aws_iam_role" "iam_for_lambda" {
   EOF
 }
 
-resource "aws_iam_role_policy_attachment" "basic_execution" {
-  role       = aws_iam_role.iam_for_lambda.id
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-
-}
-
 resource "aws_s3_object" "initial" {
   bucket                 = aws_s3_bucket.this.bucket
   key                    = "source"
@@ -131,24 +125,8 @@ resource "aws_lambda_function" "this" {
 
 # == CloudWatch == #
 
-resource "aws_iam_role_policy" "logging" {
-  count = var.enable_cloudwatch ? 1 : 0
-  role  = aws_iam_role.iam_for_lambda.id
-
-  policy = <<-EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "arn:aws:logs:*:*:*",
-        "Effect": "Allow"
-      }
-    ]
-  }
-  EOF
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  count     = var.enable_cloudwatch ? 1 : 0
+  role       = aws_iam_role.iam_for_lambda.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
